bd626363c32e2cb9ad8c8804eacc9fd5647b9ac8
AlexGthr <agauthier5@outlook.fr>
2024-02-05 13:33:22 +0100

    Ajout de commentaire main.JS

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
+// On récupère l'élément #board
 const board = document.querySelector("#board");
+
+// On crée une box avec la class "carree"
 const box = document.createElement("div")
 box.classList.add("carree")
 
+// On utilise une function pour ajouter/supprimer la classe clicked lors d'un click sur une box
 function showReaction(type, clickedBox) {
     clickedBox.classList.add(type);
     if (type == "clicked") {
@@ -11,6 +15,7 @@ function showReaction(type, clickedBox) {
     }
 }
 
+// On crée une boucle qui permettra de crée le nombre de box que l'ont souhaite (ici 4)
 for (let i = 1; i <= 4; i++) {
     const newBox = box.cloneNode(); // Clone des divs
     newBox.innerText = i; // Numéro écrit dans le bloc
